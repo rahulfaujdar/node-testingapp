44b60733621c407620b917feb0df29b12188598a
Rahul Faujdar <83919231+Rahul-Faujdar@users.noreply.github.com>
2021-07-06 16:00:12 +0530

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -23,9 +23,9 @@ app.use(function (err, req, res, next){
     res.json(error(err.message));
 });
 
-app.listen(Number(PORT), HOST, () => {
-        console.log(`listening on ${HOST}:${PORT}`);
-    });
+// app.listen(Number(PORT), HOST, () => {
+//         console.log(`listening on ${HOST}:${PORT}`);
+//     });
 
 // Promise.all([connectTestDb()]).then(() => {
 //     app.listen(Number(PORT), HOST, () => {
