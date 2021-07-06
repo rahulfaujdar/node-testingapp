3b263b56972554c9081928210facde2504e9319e
Rahul Faujdar <83919231+Rahul-Faujdar@users.noreply.github.com>
2021-07-06 16:47:05 +0530

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -23,15 +23,15 @@ app.use(function (err, req, res, next){
     res.json(error(err.message));
 });
 
-app.listen(Number(PORT), HOST, () => {
-        console.log(`listening on ${HOST}:${PORT}`);
-    });
-
-// Promise.all([connectTestDb()]).then(() => {
-//     app.listen(Number(PORT), HOST, () => {
+// app.listen(Number(PORT), HOST, () => {
 //         console.log(`listening on ${HOST}:${PORT}`);
 //     });
-//     // noinspection JSIgnoredPromiseFromCall
-//     runScheduler();
-// });
+
+Promise.all([connectTestDb()]).then(() => {
+    app.listen(Number(PORT), HOST, () => {
+        console.log(`listening on ${HOST}:${PORT}`);
+    });
+    // noinspection JSIgnoredPromiseFromCall
+    runScheduler();
+});
 
